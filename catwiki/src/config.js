788c06890237c3739c6801d8e2d0ae8bee3e5a16
LucaswithC <lucas.kiers@eclipso.at>
2023-01-31 14:54:28 +0100

    API Address

--- catwiki/src/config.js
+++ catwiki/src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 var config = {
-    SERVER_URL: "http://localhost:8080" /*"https://cat-wiki-api-fi6i.onrender.com"*/
+    SERVER_URL: "https://cat-wiki-api-fi6i.onrender.com"
 }
 
 export default config;
