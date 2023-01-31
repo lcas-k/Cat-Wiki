179c1da9aa338d386e16e704ba244169177ad3a3
LucaswithC <lucas.kiers@eclipso.at>
2023-01-31 13:49:03 +0100

    Server URL Change

--- catwiki/src/config.js
+++ catwiki/src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 var config = {
-    SERVER_URL: "https://cat-wiki-lucas.herokuapp.com"
+    SERVER_URL: "https://cat-wiki-api-fi6i.onrender.com"
 }
 
 export default config;
